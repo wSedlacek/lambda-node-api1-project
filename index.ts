d6bf39e1fc09b0f8583a54b5ad49f9004ad3f702
William Sedlacek <wsedlacekc@gmail.com>
2019-10-28 13:35:29 -0500

    Add middleware
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const server = express();
+server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
   res.send('Hello from Express');
