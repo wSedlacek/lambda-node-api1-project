ab71940488f4ad521785065b2106514e09242b67
William Sedlacek <wsedlacekc@gmail.com>
2019-10-28 13:36:25 -0500

    Setup PUT
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- index.ts
+++ index.ts
@@ -50,4 +50,26 @@ server.post('/api/users', async (req, res) => {
   }
 });
 
+server.put('/api/users/:id', async (req, res) => {
+  if (!(await validID(req.params.id))) {
+    res.status(404);
+    res.json({ message: 'The user with the specified ID does not exist.' });
+  }
+
+  if (!validUser(req.body)) {
+    res.status(400);
+    res.json({ errorMessage: 'Please provide name and bio for the user.' });
+  }
+
+  const updated = await update(req.params.id, req.body);
+  if (!updated) {
+    res.status(500);
+    res.json({ error: 'There was an error while saving the user to the database' });
+  }
+
+  const user = await findByID(req.params.id);
+  res.status(201);
+  res.json(user);
+});
+
 server.listen(5000, () => console.log('Server running on http://localhost:5000'));
